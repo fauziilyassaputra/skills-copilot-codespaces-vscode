0928c8dc7dfb52cb5c470a37b32330ab12f787b8
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-02-27 02:10:02 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,7 +1,6 @@
-function skillMember() {
-    this.name = 'skillMember';
-    this.skill = 'JavaScript';
-    this.show = function() {
-        console.log(this.name + ' has a skill in ' + this.skill);
+function skillsMember() {
+  return {
+    name: 'John Doe',
+    skills: ['JavaScript', 'React', 'Node']
     };
 }
